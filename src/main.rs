060f270d3e1a876669ea556a492f7e75268b06fb
Sergey Sova <mail@sergeysova.com>
2018-12-18 18:24:14 +0300

    fix: provide descriptable errors

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ use std::env;
 fn main() {
     dotenv().ok();
     // main env var, panic if missing
-    let ipc_path = env::var("IPC_PATH").unwrap();
-    let pg_path = env::var("PG_PATH").unwrap();
+    let ipc_path = env::var("IPC_PATH").expect("IPC_PATH env var not provided");
+    let pg_path = env::var("PG_PATH").expect("IPC_PATH env var not provided");
 
     let mut pipe = Pipe::new(&ipc_path, &pg_path).unwrap();
     pipe.run().unwrap();
